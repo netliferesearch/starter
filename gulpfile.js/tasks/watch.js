be7f85740b035046aca8d0774a5677eeb4a18310
Fredrik Jensen <fredrik@netliferesearch.com>
2015-12-05 21:28:54 +0100

    nunjucks fix

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -5,9 +5,9 @@ var gulp = require('gulp'),
 
 gulp.task('watch', function() {
     gulp.watch(config.src.scss, ['sass']);
-    gulp.watch(config.src.scss, [ 'scripts' ]).on('change', browserSync.reload);
-    gulp.watch(config.src.layout, ['nunjucks']);
-    gulp.watch(config.src.pages, ['nunjucks']);
+    gulp.watch(config.src.js, [ 'scripts' ]).on('change', browserSync.reload);
+    gulp.watch(config.src.layout, ['nunjucks']).on('change', browserSync.reload);
+    gulp.watch(config.src.pages, ['nunjucks']).on('change', browserSync.reload);
     gulp.watch(config.src.styleguide, ['styleguide']);
     gulp.watch(config.src.images, ['images']);
 })
