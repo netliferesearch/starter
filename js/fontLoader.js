bfb0c600ace0e7450ac66f6d937aecf45c199945
Fredrik Jensen <fredrik@netliferesearch.com>
2015-12-15 19:59:50 +0100

    added webfontloader

---
+++ js/fontLoader.js
@@ -0,0 +1,21 @@
+
+WebFont.load({
+  custom: {
+    families: ['My Font'],
+    //urls: ['/fonts.css'] Local loading
+  },
+  timeout: 2000 // Set the timeout to two seconds
+});
+
+/*
+WebFontConfig = {
+  google: {
+    families: ['Open Sans Condensed:300,700']
+  }
+};
+WebFontConfig = {
+  typekit: {
+    id: 'xxxxxx'
+  }
+};
+*/
