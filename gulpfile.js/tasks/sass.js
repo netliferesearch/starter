1b05e75148ebdca1698db2757c2a6b767c157f0c
Fredrik Jensen <fredrik@netliferesearch.com>
2015-12-19 17:45:14 +0100

    Fixed sass sourcemap and font size iteration

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -11,27 +11,28 @@ var gulp = require('gulp'),
     gutil = require('gulp-util');
 
 gulp.task('sass', function() {
-    return gulp.src(config.src.scss)
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            style: 'expanded'
-            })
-            .on('error', function(err){
-                browserSync.notify(err.message, 10000);
-                this.emit('end');
-            })
-            .on('error', sass.logError)
-        )
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest(config.dist.css))
-        .pipe(minifyCSS())
-        .pipe(rename('main.min.css'))
-        .pipe(gulp.dest(config.dist.css))
-        .pipe(sass({errLogToConsole: true}))
-        .pipe(browserSync.reload({stream: true}))
+    return gulp.src(config.src.scss )
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        style: 'expanded'
+        })
+        .on('error', function(err){
+            browserSync.notify(err.message, 10000);
+            this.emit('end');
+        })
+        .on('error', sass.logError)
+      )
+      .pipe(sourcemaps.write())
+      .pipe(autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+      }))
+      .pipe(gulp.dest(config.dist.css))
+      .pipe(minifyCSS())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest(config.dist.css))
+      .pipe(sass({errLogToConsole: true}))
+      .pipe(browserSync.reload({stream: true}))
 });
