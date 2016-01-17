e95636a975e96d063464ba9dc7954c1b50e58acb
Fredrik Jensen <fredrik@netliferesearch.com>
2016-01-17 21:08:32 +0100

    successfully removed bower

--- js/webFontLoader.js
+++ js/webFontLoader.js
@@ -1,5 +1,4 @@
-
-var WebFontConfig = {
+WebFont.load({
 
   google: {
     families: ['Work Sans:300,400,600,700', 'Lora']
@@ -19,13 +18,4 @@ var WebFontConfig = {
   */
  timeout: 2000 // Set the timeout to two seconds
 
-};
-
-// asynchronous loading of Webfontloader
-(function(){
-	var wf = document.createElement("script");
-	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-	wf.async = 'true';
-	document.head.appendChild(wf);
-})();
+});
