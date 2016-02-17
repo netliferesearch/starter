e20f23566add8e09647572c3c994891ea68d127d
Fredrik Jensen <fredrik@netliferesearch.com>
2016-02-17 23:52:12 +0100

    testing modifing tasks

--- gulpfile.js/tasks/index.js
+++ gulpfile.js/tasks/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
-module.exports = {
-    'build': [['assets', 'browserify', 'sass']],
-	'default': [['build', 'browser-sync', 'watch']],
-	'deploy': [['assets', 'build', 'sass', 'gh-pages']]
-};
+var gulp = require('gulp');
+var starterGulp = require('starter-gulp');
+
+starterGulp.extendTasks(gulp, {
+    ownBuild: [
+            ['sass']
+    ]
+});
