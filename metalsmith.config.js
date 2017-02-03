440f9ee84cb739f8a2981d3adfd745f9a69151b2
Martin Berglund <martinb@netliferesearch.com>
2017-02-03 10:50:01 +0100

    Reorganize files

--- metalsmith.config.js
+++ metalsmith.config.js
@@ -2,19 +2,19 @@
 module.exports = {
     port: 43565,
     src: {
-        content: './src/html/content',
-        layout: './src/html/layouts',
-        partials: './src/html/layouts/partials',
-        icons: './src/icons/svg/*.svg',
-        assets: './src/images',
+        content: './src/pages',
+        layout: './src/layouts',
+        partials: './src/partials',
+        icons: './src/assets/icons/svg/*.svg',
+        assets: './src/assets/images',
         watch: {
             '${source}/**/*': '**/*',
-            './src/html/layouts/**/*': '**/*',
+            './src/layouts/**/*': '**/*',
         },
     },
     dist: {
         root: 'dist',
-        assets: './images',
+        assets: './assets/images',
         icons: 'src/html/svg-icons',
         html: './dist/',
 
